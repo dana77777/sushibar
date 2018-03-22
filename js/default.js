402bb17602e9511c6e7a43d0d330a68d8dace615
dana77777 <37642352+dana77777@users.noreply.github.com>
2018-03-21 20:36:27 -0400

    Initial commit

---
+++ js/default.js
@@ -0,0 +1,9 @@
+dyscrollup.init({
+    showafter:600,
+    scrolldelay: 1000,
+    position: "right",
+    image: "plugins/dyScrollUpJS-1.1.4/image/48-red.png",
+    shape: "circle",
+    width: 30,
+    height: 30
+});
